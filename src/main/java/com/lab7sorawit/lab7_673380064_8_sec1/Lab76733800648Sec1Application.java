0d1b300ea76bdef0cb81d31219527cdf27ba659a
PoohSorawit <hillingstudio@gmail.com>
2026-08-08 20:59:51 +0700

    first commit

---
+++ src/main/java/com/lab7sorawit/lab7_673380064_8_sec1/Lab76733800648Sec1Application.java
@@ -0,0 +1,13 @@
+package com.lab7sorawit.lab7_673380064_8_sec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab76733800648Sec1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab76733800648Sec1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab7sorawit/lab7_673380064_8_sec1/Lab76733800648Sec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab7sorawit.lab7_673380064_8_sec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab76733800648Sec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
